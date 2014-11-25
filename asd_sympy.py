7503d560dfbbb145d12cf11c1f4761f737dbacd9
mobeets <mobeets@gmail.com>
2014-11-25 16:08:48 -0600

    new intuition graph

---
+++ asd_sympy.py
@@ -0,0 +1,8 @@
+"""
+http://docs.sympy.org/latest/tutorial/intro.html
+
+TODO: implement evidence expression, and try to integrate/differentiate?
+"""
+import sympy
+R, L = sympy.symbols('R L')
+evidence = R + L
